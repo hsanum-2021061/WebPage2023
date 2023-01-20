2c81a4ca430ff72739bb7799930d2579211d793b
Henry Sanum <hsanum-2021061@kinal.edu.gt>
2023-01-20 09:32:09 -0600

    V1 commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function saludar(){
+    let campo1 = document.getElementById('campo1').value;
+    let campo2 = document.getElementById('campo2').value;
+
+    let resultado =  parseInt (campo1) + parseInt (campo2);
+
+    console.log('saludar');
+    console.log(resultado);
+}
+
+
+console.log('Good Morning');
